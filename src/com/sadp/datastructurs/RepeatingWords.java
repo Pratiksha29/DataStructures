b485988304613e647de869a080fbb75448136eb0
psha71 <psha71@BLRNAL13.sapient.com>
2017-05-25 10:01:15 +0530

    HashMap

---
+++ src/com/sadp/datastructurs/RepeatingWords.java
@@ -0,0 +1,20 @@
+package com.sadp.datastructurs;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RepeatingWords {
+
+	public static void main(String[] args) {
+		String sample = "This is sapient.This is bangalore";
+		Map<String,Integer> map = new HashMap<>();
+		String[] words = sample.split(" ");
+		for (String word : words) {
+			if(map.put(word, 1)!=null){
+				map.put(word, map.get(word)+1);
+			}
+		}
+		System.out.println(map);
+	}
+
+}
---
+++ src/com/sadp/datastructurs/SetImple.java
@@ -0,0 +1,10 @@
+package com.sadp.datastructurs;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetImple {
+public static void main(String[] args) {
+	Set set =new HashSet<>();
+}
+}
